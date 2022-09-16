196af787317371a22acc30e4602770d140b2a6c6
unknown <dokunari@i.softbank.jp>
2022-09-16 14:42:36 +0900

    プロジェクト新規作成

---
+++ popo/src/main/java/com/example/popo/PopoApplication.java
@@ -0,0 +1,14 @@
+package com.example.popo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopoApplication.class, args);
+		
+	}
+
+}
